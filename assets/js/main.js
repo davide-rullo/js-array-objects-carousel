2bd9e379f712253a0fe3b89a5fab7837b96a10fd
davide-rullo <davide.rullo01@gmail.com>
2023-09-07 17:18:31 +0200

    refactoring
    refactoring js with functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,104 +102,25 @@ slides.forEach(function (slide, index) {
 
 
 
-
+//functions to control carousel
 
 
 // intercept click on the next icon 
-nextEl.addEventListener('click', function () {
-    console.log('cliccato su next');
-
-    // select the current slide
-    const currentSlide = slidesImages[activeSlide]
-    console.log("la slide corrente è " + currentSlide);
-    // remove the active class from the current slide
-    currentSlide.classList.remove('active')
-
-    // select the active thumb
-    const currentThumb = document.querySelector('.thumbnails > img.active')
-    console.log(currentThumb);
-    // remove the active class from the active thumb
-    currentThumb.classList.remove('active')
-
-
-    // activeSlide = 4
-
-    if (activeSlide === slidesImages.length - 1) {
-        activeSlide = 0
-        // activeSlide = 5
-    } else {
-        // increment the activeSlide of 1
-        activeSlide++
-    }
-
-
-    // select the next slide
-    const nextSlide = slidesImages[activeSlide]
-    console.log(nextSlide);
-    // add the active class to the next slide
-    nextSlide.classList.add('active')
-
-
-    /* TODO */
-
-
-    // select the next thumb
-    const nextThumb = document.querySelectorAll('.thumb')[activeSlide]
-    console.log(nextThumb);
-    // add to the next thumb the active class
-    nextThumb.classList.add('active')
-
-
-})
+nextEl.addEventListener('click', goNext)
 
 // intercept click on the prev icon
 
 
 // activeSlide = 0
-prevEl.addEventListener('click', function () {
-    console.log('cliccato su prev');
-
-
-    // select the current slide
-    const currentSlide = slidesImages[activeSlide]
-    console.log(currentSlide);
-    // remove the active class from the current slide
-    currentSlide.classList.remove('active')
-
-    // select the active thumb
-    const currentThumb = document.querySelector('.thumbnails > img.active')
-    currentThumb.classList.remove('active')
-
-    if (activeSlide === 0) {
-        activeSlide = slidesImages.length - 1
-        // activeSlide = 5
-    } else {
-        // decrement the activeSlide of 1
-        activeSlide--
-    }
-
-
-    console.log(activeSlide);
-
-
-    // select the next slide
-    const nextSlide = slidesImages[activeSlide]
-    console.log(nextSlide);
-    // add the active class to the next slide
-    nextSlide.classList.add('active')
-
-    // select the next thumb
-    const nextThumb = document.querySelectorAll('.thumb')[activeSlide]
-    console.log(nextThumb);
-    // add to the next thumb the active class
-    nextThumb.classList.add('active')
-
-})
+prevEl.addEventListener('click', goBack)
 
 
 //carosello che scorre in automatico
 
-setInterval(function() {
+setInterval(goNext, 5000);
+
+
+function goNext(){
     const currentSlide = slidesImages[activeSlide]
     console.log("la slide corrente è " + currentSlide);
     
@@ -222,7 +143,41 @@ setInterval(function() {
     const nextThumb = document.querySelectorAll('.thumb')[activeSlide]
     nextThumb.classList.add('active')
 
+}
 
-}, 5000);
+function goBack(){
+ // select the current slide
+ const currentSlide = slidesImages[activeSlide]
+ console.log(currentSlide);
+ // remove the active class from the current slide
+ currentSlide.classList.remove('active')
+
+ // select the active thumb
+ const currentThumb = document.querySelector('.thumbnails > img.active')
+ currentThumb.classList.remove('active')
+
+ if (activeSlide === 0) {
+     activeSlide = slidesImages.length - 1
+     // activeSlide = 5
+ } else {
+     // decrement the activeSlide of 1
+     activeSlide--
+ }
 
 
+ console.log(activeSlide);
+
+
+ // select the next slide
+ const nextSlide = slidesImages[activeSlide]
+ console.log(nextSlide);
+ // add the active class to the next slide
+ nextSlide.classList.add('active')
+
+ // select the next thumb
+ const nextThumb = document.querySelectorAll('.thumb')[activeSlide]
+ console.log(nextThumb);
+ // add to the next thumb the active class
+ nextThumb.classList.add('active')
+
+}
